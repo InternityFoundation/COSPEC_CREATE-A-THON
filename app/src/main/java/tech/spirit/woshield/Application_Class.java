1a54889db3adf01486c3cfb01b0f0bb42175e63d
techspiriteagle <techspiriteagle@gmail.com>
2019-07-07 01:55:54 +0530

    Map with current location update added

--- app/src/main/java/tech/spirit/woshield/Application_Class.java
+++ app/src/main/java/tech/spirit/woshield/Application_Class.java
@@ -2,9 +2,13 @@ package tech.spirit.woshield;
 
 import android.app.Application;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Application_Class extends Application {
+    public static double lat, longi;
+    public static Address_Location address_location;
+    public static LatLng location=new LatLng(23.23,24.3);
 
 public static FirebaseUser firebaseUser;
     @Override
--- app/src/main/java/tech/spirit/woshield/MainActivity.java
+++ app/src/main/java/tech/spirit/woshield/MainActivity.java
@@ -1,14 +1,191 @@
 package tech.spirit.woshield;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static tech.spirit.woshield.Application_Class.address_location;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private static final int REQUEST_PERMISSIONS = 100;
+    boolean boolean_permission;
+    TextView tv_latitude, tv_longitude, tv_address,tv_area,tv_locality;
+    SharedPreferences mPref;
+    SharedPreferences.Editor medit;
+    Double latitude,longitude;
+    Geocoder geocoder;
+
+
+
+    Button btnlocation,showOnMap;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnlocation=findViewById(R.id.btnlocation);
+        showOnMap=findViewById(R.id.show);
+
+        geocoder = new Geocoder(this, Locale.getDefault());
+        mPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        medit = mPref.edit();
+
+        fn_permission();
+
+
+
+        btnlocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(boolean_permission) {
+                    Intent intent = new Intent(getApplicationContext(), GoogleService.class);
+                    startService(intent);
+                }
+
+
+//                    startActivity(new Intent(MainActivity.this,MapsActivity.class));
+//                    if (mPref.getString("service", "").matches("")) {
+////                        medit.putString("service", "service").commit();
+//
+//
+//
+//                    } else {
+//                        Toast.makeText(getApplicationContext(), "Service is already running", Toast.LENGTH_SHORT).show();
+//
+//
+//                    }
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "Please enable the gps", Toast.LENGTH_SHORT).show();
+//                }
+                }
+
+
+        });
+
+
+        showOnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MapsActivity.class));
+            }
+        });
     }
+
+    private void fn_permission() {
+        if ((ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+
+            if ((ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION))) {
+
+
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION
+
+                        },
+                        REQUEST_PERMISSIONS);
+
+            }
+        } else {
+            boolean_permission = true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    boolean_permission = true;
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please allow the permission", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        }
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            latitude = Double.valueOf(intent.getStringExtra("latutide"));
+            longitude = Double.valueOf(intent.getStringExtra("longitude"));
+            Application_Class.location=new LatLng(latitude,longitude);
+            Log.d("location",""+latitude+longitude);
+            List<Address> addresses = null;
+
+            try {
+                addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                String cityName = addresses.get(0).getAddressLine(0);
+                String stateName = addresses.get(0).getAddressLine(1);
+                String countryName = addresses.get(0).getAddressLine(2);
+//
+//                tv_area.setText(addresses.get(0).getAdminArea());
+//                tv_locality.setText(stateName);
+//                tv_address.setText(countryName);
+
+                Log.d("Address", cityName + "" + countryName + "" + stateName);
+
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+
+//            tv_latitude.setText(latitude+"");
+//            tv_longitude.setText(longitude+"");
+//            tv_address.getText();
+
+
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(broadcastReceiver, new IntentFilter(GoogleService.str_receiver));
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcastReceiver);
+    }
+
+
 }
